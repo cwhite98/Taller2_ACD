ddd3124ea960e034fc5abf5e7679e0ca6579fb23
Douglas Ardila Garcés <dag1702@hotmail.com>
2020-09-13 20:41:46 -0500

    punto 8f

---
+++ punto8f.m
@@ -0,0 +1,25 @@
+%% lectura de archivo
+data = readtable('portfolio100.txt');
+datarray = data{:,:};
+A = datarray(1:100, 2: end);
+
+A_cov = cov(A);
+[filas, col] = size(A);
+A_barra = A - ((1/filas) * ones(filas) * ones(filas)' * A);
+mediana = median(A);
+P = eye(filas) - (ones(filas) * ones(filas)' / filas);
+A_centrado = A * P;
+
+%% descomposicion valores singulares de A
+[U, S, V] = svd(A);
+
+%% descomposicion espectral de A_barra
+[auto_vector_A_barra, auto_valor_A_barra] = eig(A_barra);
+des_espectral = auto_vector_A_barra * auto_valor_A_barra * auto_vector_A_barra';
+
+%% norma matriz
+norma = norm(A, 2)
+
+%% Conclusion:
+% la norma 2 de la matriz A es la misma que el mayor valor singular de la
+% matriz A
