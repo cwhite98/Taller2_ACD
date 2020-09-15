41d3eabd3f0a9376404c56194b7bbf87b375b80e
Douglas Ardila Garcés <dag1702@hotmail.com>
2020-09-14 20:59:35 -0500

    varianzas

--- punto12.m
+++ punto12.m
@@ -1,35 +1,42 @@
-% lectura portfolio100
+%% lectura portfolio100
 data = readtable('portfolio100.txt');
 datarray = data{:,:};
 date = datarray(:, 1);
 datarray = datarray(1:500, 2: end);
 
-% calculo asimetria
-asimetria = skewness(datarray);
+%% calculo asimetria
+asimetria = skewness(datarray');
 
-% calculo curtosis
-curtosis = kurtosis(datarray);
+%% calculo curtosis
+curtosis = kurtosis(datarray');
 
-% calculo varianza global
-var_global = var(datarray);
+%% calculo varianza global
+var1 = var(datarray');
+var_global = sum(var1);
 
-% calculo varianza promedio
+%% calculo varianza promedio
+var_prom = mean(var1);
 
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% columna de promedio
+%% columna de promedio
 datarray(:, 101) = mean(datarray')';
 
-% calculo asimetria
-asimetria_ = skewness(datarray);
+%% calculo asimetria con promedio
+asimetria_prom = skewness(datarray');
 
-% calculo curtosis
-curtosis_ = kurtosis(datarray);
+%% calculo curtosis con promedio
+curtosis_prom = kurtosis(datarray');
 
-% calculo varianza global
-var_global_ = var(datarray);
+%% calculo varianza global con promedio
+var2 = var(datarray');
+var_global2 = sum(var2);
 
-% calculo varianza promedio
+%% calculo varianza promedio con promedio
+var_prom2 = mean(var2);
 
-
-
+%% revision de diferencias entre los resultados de ambas matrices
+% coger norma 1 entre los 2 vectores, si es muy cercano a 0 es que se
+% parecen mucho
+norma_asimetria = norm(asimetria_prom - asimetria, 1);
+norma_curtosis = norm(curtosis_prom - curtosis, 1);
+dif_var_global = abs(var_global2 - var_global);
+dif_var_prom = abs(var_prom2 - var_prom);
