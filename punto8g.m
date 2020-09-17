0f981eed698f8dc28d046d6948d6608b23df585e
Camila White <camilawhite98@gmail.com>
2020-09-16 19:45:28 -0500

    Add files via upload

--- punto8g.m
+++ punto8g.m
@@ -1,10 +1,11 @@
-%%%% A vs A+I %%%%%
+
 n = randi([2 5]);
 p = n;
 
 vec1 = ones([n,1]);
 A = rand(p,p);
 
+%%%% A vs A+I %%%%%
 AGorro = A-(1/n)*vec1*vec1'*A;
 
 [vecAGorro, valsAGorro] = eig(AGorro);
